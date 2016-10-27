60ed5f2ec1cba62e57db263ad7d595da0dc366c2
Adam Lischinsky <adamlischinsky@gmail.com>
2016-10-28 01:12:50 +0200

    20 depth

--- src/myPackage/UCI.java
+++ src/myPackage/UCI.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class UCI {
 	static Game currGame = new Game();
 	static int num_moves;
-	static final int DEPTH = 4;
+	static final int DEPTH = 3;
 	static final boolean QUIESCE = false;
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
