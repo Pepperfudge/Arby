d54b9d6b65529b92878657fd0555f9465f2cabfd
Adam Lischinsky <adamlischinsky@gmail.com>
2016-11-02 22:02:37 +0100

    Improved ordering: 4 levels of search, 2 levels of quiescence

---
+++ src/myPackage/TimingTest.java
@@ -0,0 +1,14 @@
+package myPackage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TimingTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
